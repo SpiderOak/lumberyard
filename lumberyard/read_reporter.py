21253f8f97222fa23700983ae5287429134fa25e
Doug Fort <dougfort@dougfort.com>
2011-08-10 09:44:26 -0400

    add read_reporter for reporting archive status

---
+++ lumberyard/read_reporter.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+read_reporter.py
+
+class ReadReporter
+
+A class that wraps a python file object (or something like him)
+and reports bytes read to a callback.
+
+Intended to be passed as the 'body' parameter to HTTPConnection.request
+
+Takes an open python file object and a callback functions as arguments
+The callback should take a single integer as an argument: bytes_read.
+"""
+import logging
+
+class ReadReporter(object):
+    """
+    A class that wraps a python file object (or something like him)
+    and reports bytes read to a calback
+
+    Intended to be passed as the 'body' parameter to HTTPConnection.request
+
+    Takes an open pythong file object and a callback functions as arguments
+    The callback should take a single integer as an argument: bytes_read.
+    """
+    def __init__(self, file_object, callback=None):
+        self._log = logging.getLogger("ReadReporter")
+        self._file_object = file_object
+        self._callback = callback
+
+    def set_callback(self, callback):
+        self._callback = callback
+
+    def close(self):
+        self._log.debug("close")
+        self._file_object.close()
+
+    def fileno(self):
+        self._log.debug("fileno = %s"% (self._file_object.fileno(), ))
+        return self._file_object.fileno()
+
+    def read(self, size=None):
+        self._log.debug("read(%r)" % (size, ))
+        data = None
+        if size is None:
+            data = self._file_object.read()
+        else:
+            data = self._file_object.read(size)
+
+        self._log.debug("actual bytes read %s" % (len(data), ))
+        self._callback(len(data))
+
+        return data
+
+    def seek(self, offset, whence=None):
+        self._log.debug("seek(%r, %r)" % (offset, whence, ))
+        self._file_object.seek(offset, whence)
+
+    def tell(self):
+        result = self._file_object.tell()
+        self._log.debug("tell() = %s" % (result, ))
+        return result
+
