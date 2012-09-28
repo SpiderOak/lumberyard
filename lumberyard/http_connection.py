21ed97339daef8b092698654cfe07dfe754b0c09
dougfort <dougfort@spideroak.com>
2012-09-28 15:08:24 -0400

    Add Unauth connection, mostly for testing access_control

--- lumberyard/http_connection.py
+++ lumberyard/http_connection.py
@@ -188,3 +188,122 @@ class HTTPConnection(_base_class):
 
         return response
 
+class UnAuthHTTPConnection(_base_class):
+    """
+    base_address
+        A hostname or address that should resolve to a socket connection on a 
+        server.
+
+        For example ``nimbus.io`` or ``127.0.0.1:8088``
+
+    debug level
+        debug level of HTTPConnection base class 
+
+    nimbus.io wrapper for httplib.HTTPSConnection. This constructor performs
+    the initial connection but does not send a request.
+    """
+    def __init__(
+        self, 
+        base_address, 
+        debug_level=0
+    ):
+        _base_class.__init__(self, base_address, timeout=_timeout)
+        self._log = logging.getLogger("UnAuthHTTPConnection")
+        self.set_debuglevel(debug_level)
+        self.connect()
+
+    def request(self, 
+                method, 
+                uri, 
+                body=None, 
+                headers=None, 
+                expected_status=httplib.OK):
+        """
+        method
+            one of GET, POST, DELETE, HEAD
+
+        uri
+            the REST command for this request
+
+        body
+            if present, must be a string or an open file object
+
+        headers
+            a dictionary of key/value pairs to be added to the HTTP headers
+
+        expected_status
+            status indicating a successful result, for example 201 CREATED
+
+        send an HTTP request over the connection
+        return a HTTPResponse object, or raise an exception
+        """
+        if headers is None:
+            headers = dict()
+
+        headers.update({
+            "agent"                 : 'lumberyard/1.0'
+        })
+
+        # nginx will (rightly) reject with HTTP status 411 Length Required if
+        # we send a PUT without a Content-Length header, even if the body size
+        # is 0.  and our httplib base class won't add this header if body is
+        # None.
+        if method == "PUT":
+            try:
+                headers.setdefault("Content-Length", bytes(len(body)))
+            except TypeError:
+                if body is None:
+                    headers.setdefault("Content-Length", bytes(0))
+
+        # the body maybe beyond 7 bit ascii, so a unicode URL can't be combined
+        # with it into a single string to form the request.
+        uri = bytes(uri)
+
+        try:
+            _base_class.request(self, method, uri, body=body, headers=headers)
+        except Exception, instance:
+            # 2012-02-14 dougfort -- we are getting timeouts here
+            # on heavily loaded benchmark tests
+            self._log.exception(str(instance))
+            self.close()
+            raise LumberyardHTTPError(500, str(instance))
+
+        try:
+            response = self.getresponse()
+        except httplib.BadStatusLine, instance:
+            self._log.exception("BadStatusLine: '{0}'".format(instance))
+            self.close()
+            raise LumberyardHTTPError(500, "BadStatusLine")
+
+        if response.status != expected_status:
+            self._log.error("request failed %s %s" % (
+                response.status, response.reason, 
+            )) 
+
+            # the server may have closed the connection already if this is an
+            # error response
+            try:
+                response.read()
+            except socket.error:
+                pass
+
+            self.close()
+
+            # if we got 503 service unavailable
+            # and there is a retry_after header with an integer value 
+            # give the caller a chance to retry
+            if response.status == 503:
+                retry_after = response.getheader("RETRY-AFTER", None)
+                if retry_after is not None:
+                    seconds = None
+                    try:
+                        seconds = int(retry_after)
+                    except Exception:
+                        pass
+                    if seconds is not None and seconds > 0:
+                        raise LumberyardRetryableHTTPError(seconds)
+
+            raise LumberyardHTTPError(response.status, response.reason)
+
+        return response
+
